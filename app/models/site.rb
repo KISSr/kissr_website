dc242a4af09e5e864ddd163d8b5819be386c0e03
Mason Fischer <mason@thoughtbot.com>
2014-05-20 12:47:09 -0700

    Add a bootstrap site

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,9 +3,23 @@ class Site < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   after_save :update_s3_webhooks
+  after_save :create_dropbox_folder
 
   def domain
     "#{name}.kissr.com"
+  end
+
+  def create_dropbox_folder
+    Dir["#{Rails.root}/templates/default/**/**"].each do |file|
+      unless File.directory?(file)
+        destination_path= "#{domain}/#{file.sub("#{Rails.root}/templates/default","")}"
+        dropbox.put_file(destination_path, File.new(file,'r'))
+      end
+    end
+  end
+
+  def dropbox
+    @dropbox ||= DropboxClient.new(user.token)
   end
 
   def update_s3_webhooks
