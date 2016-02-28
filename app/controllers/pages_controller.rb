c806d066debde0d67aac38f9cd20e0ab8abc914e
Mason Fischer <mason@thoughtbot.com>
2016-02-28 11:29:44 -0600

    Redesign

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+  layout :layout_for_page
 
   before_filter :redirect_if_logged_in
 
@@ -8,6 +9,15 @@ class PagesController < ApplicationController
   def redirect_if_logged_in
     if params[:id] == 'home' && signed_in?
       redirect_to sites_path
+    end
+  end
+
+  def layout_for_page
+    case params[:id]
+    when 'home'
+      'home'
+    else
+      'application'
     end
   end
 end
