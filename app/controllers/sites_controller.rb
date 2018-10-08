b89ecd50258714e62599e9ca97fceee71f89b3fa
Mason Fischer <mason@kissr.co>
2018-10-08 07:59:10 -0500

    ban mail.ch

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -40,6 +40,7 @@ class SitesController < ApplicationController
   def block_cabal
     if current_user.email.include?("cabal") ||
         current_user.email.include?("post.cz") ||
+        current_user.email.include?("mail.ch") ||
         current_user.email.include?("astridnatalia") ||
         current_user.email.include?("peopplemail") ||
         current_user.email.include?("solacestore") ||
