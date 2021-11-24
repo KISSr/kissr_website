82cfc4ca8a43b6806e09f5abc1b199d12898f9c7
Mason Fischer <mason@kissr.co>
2021-11-24 15:24:57 -0500

    Configure Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,9 @@ NewKissr::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
+    :user_name => "apikey",
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'kissr.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
