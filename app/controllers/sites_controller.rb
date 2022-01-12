e426b525bb1b9d66536756ef3ab4617f401ce680
Mason Fischer <mason@kissr.co>
2022-01-12 08:11:40 -0500

    Ban nikost92@gmail.com

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -68,6 +68,7 @@ class SitesController < ApplicationController
         current_user.email.include?("mail.ch") ||
         current_user.email.include?("mayparadis111@gmail.com") ||
         current_user.email.include?("milad13791379milad@gmail.com") ||
+        current_user.email.include?("nikost92@gmail.com") ||
         current_user.email.include?("north8247@gmail.com") ||
         current_user.email.include?("olegsergey@ro.ru") ||
         current_user.email.include?("pacmanpacman@mail.com") ||
